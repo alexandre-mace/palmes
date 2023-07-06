30b513b0f9e97ecf5cf6259de709f4bffe6aecb9
alexandre-mace <helloalexandremace@protonmail.com>
2023-07-06 11:35:35 +0200

    v1

--- app/page.tsx
+++ app/page.tsx
@@ -1,112 +1,101 @@
+"use client"
+
 import Image from 'next/image'
+import {useEffect, useState} from "react";
+import filmsPalmeDOr from './../domain/movies.json'
+import JSConfetti from 'js-confetti'
+const jsConfetti = new JSConfetti()
+
+interface Movie {
+    title: string,
+    watched: boolean,
+    year: number
+}
 
 export default function Home() {
+    const [filmList, setFilmList] = useState<Movie[]>([]);
+    const [watchedFilms, setWatchedFilms] = useState<string[]>([]);
+    const [randomFilm, setRandomFilm] = useState<Movie|null>(null);
+
+    useEffect(() => {
+        const storedWatchedFilms = JSON.parse(localStorage.getItem('watchedFilms') || '{}')
+        if (storedWatchedFilms) {
+            setWatchedFilms(storedWatchedFilms);
+        }
+
+        const films = filmsPalmeDOr.map(film => ({
+            ...film,
+            watched: storedWatchedFilms.includes(film.title)
+        }));
+        setFilmList(films);
+    }, []);
+
+    useEffect(() => {
+        localStorage.setItem('watchedFilms', JSON.stringify(watchedFilms));
+    }, [watchedFilms]);
+
+    const toggleFilmWatched = (title: String) => {
+        const updatedFilms = filmList.map(film => {
+            if (film.title === title) {
+                return { ...film, watched: !film.watched };
+            }
+            return film;
+        });
+        setFilmList(updatedFilms);
+
+        const updatedWatchedFilms = updatedFilms
+            .filter(film => film.watched)
+            .map(film => film.title);
+        setWatchedFilms(updatedWatchedFilms);
+    };
+
+    const getRandomFilm = () => {
+        const unseenFilms = filmList.filter(film => !film.watched);
+        if (unseenFilms.length > 0) {
+            const randomIndex = Math.floor(Math.random() * unseenFilms.length);
+            setRandomFilm(unseenFilms[randomIndex]);
+            jsConfetti.addConfetti({
+                emojis: ['🎬', '🍿', '📽️', '🎞️', '📀', '🕴️', '🎥'],
+            })
+        } else {
+            setRandomFilm(null);
+        }
+    };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+    <main className={"container mx-auto mt-4 px-2"}>
+      <div>
+        <h1 className={"mb-10 text-3xl tracking-tight mx-auto text-center"}>Liste des Palmes d&apos;Or</h1>
+          <div className="text-center mb-10">
+              <button className={"inline-flex items-center justify-center rounded-md font-medium focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 bg-black text-white shadow hover:bg-black/90 h-9 px-4 py-2"} onClick={getRandomFilm}>On regarde quoi ? 👀</button>
+          </div>
+          {randomFilm && (
+              <div className={"mb-10 text-center"}>
+                  <h2>Ce soir, on regarde :</h2>
+                  <p className={"font-medium text-xl mt-3"}>{randomFilm.title}</p>
+                  <div className={"text-slate-600 text-md"}>{randomFilm.year}</div>
+              </div>
+          )}
+        <ul className={"grid grid-cols-2 md:grid-cols-5 gap-3"}>
+          {filmList.map(film => (
+              <li key={film.title} className={"bg-slate-100 p-4 rounded-xl text-center"}>
+                  <div>{film.title}</div>
+                  <div className={"text-slate-600 text-sm mb-2"}>{film.year}</div>
+                  <div className={"text-center"}>
+                      {film.watched &&
+                          <button onClick={() => toggleFilmWatched(film.title)} className={"inline-flex items-center justify-center text-sm rounded-md font-medium focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 bg-slate-700 text-white hover:bg-black/90 h-7 px-3 py-1"}>
+                              Vu 👀
+                          </button>
+                      }
+                      {!film.watched &&
+                          <button onClick={() => toggleFilmWatched(film.title)} className={"inline-flex items-center justify-center text-sm rounded-md font-medium focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 bg-black text-white hover:bg-black/90 h-7 px-3 py-1"}>
+                              À voir 🍿
+                          </button>
+                      }
+                  </div>
+              </li>
+          ))}
+        </ul>
       </div>
     </main>
   )
